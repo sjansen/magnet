01a1fea965a76bc0e1c79dd544c77c19fdffda12
Stuart Jansen <sjansen@buscaluz.org>
2021-03-06 06:46:21 -0700

    add ecr lifecycle policy

--- terraform/modules/bootstrap/ecr.tf
+++ terraform/modules/bootstrap/ecr.tf
@@ -1,3 +1,35 @@
+resource "aws_ecr_lifecycle_policy" "webui" {
+  repository = aws_ecr_repository.webui.name
+  policy     = <<EOF
+{
+    "rules": [{
+        "rulePriority": 10,
+        "description": "Expire untagged images after 3 days",
+        "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 3
+        },
+        "action": {
+            "type": "expire"
+        }
+    }, {
+        "rulePriority": 100,
+        "description": "Keep last 3 tagged images",
+        "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 3
+        },
+        "action": {
+            "type": "expire"
+        }
+    }]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "webui" {
   name = "${var.dns-name}-webui"
   tags = var.tags
